fdaae07f06f1fdb385d9eb9c318566f2b4e25481
chularansi <chularansifernando@yahoo.com>
2018-07-10 08:25:45 +0200

    add filter method to repository component

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IRepository } from './repository';
 
 @Component({
   selector: 'app-repository',
@@ -7,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoryComponent implements OnInit {
 
-  constructor() { }
+  repositories: IRepository[] = [
+    {
+      "id": 364,
+      "name": "acts_as_geocodable",
+      "description": "Simple geocoding for Active Record models",
+      "html_url": "https://github.com/collectiveidea/acts_as_geocodable",
+      "clone_url": "https://github.com/collectiveidea/acts_as_geocodable.git",
+      "language": "Ruby",
+    }
+  ]
+
+  _repoFilter: string;
+  filteredRepos: IRepository[];
+
+  get repoFilter(): string {
+    return this._repoFilter;
+  }
+  set repoFilter(value: string) {
+    this._repoFilter = value;
+    this.filteredRepos = this.repoFilter ? this.performFilter(this.repoFilter) : this.repositories;
+  }
+
+  performFilter(filterBy: string): IRepository[] {
+    var filterBy = filterBy.toLocaleLowerCase();
+    return this.repositories.filter((repo: IRepository) => 
+      repo.name.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  constructor() {
+    this.filteredRepos = this.repositories;
+  }
 
   ngOnInit() {
   }
