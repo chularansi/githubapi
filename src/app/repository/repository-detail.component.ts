70588e19a8b3901be70750216ccfb942b119c83e
chularansi <chularansifernando@yahoo.com>
2018-07-11 16:28:31 +0200

    add functions to repository detali component

--- src/app/repository/repository-detail.component.ts
+++ src/app/repository/repository-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IRepository } from './repository';
+import { RepositoryService } from './repository.service';
 
 @Component({
   selector: 'app-repository-detail',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repository-detail.component.css']
 })
 export class RepositoryDetailComponent implements OnInit {
+  errorMessage: string;
+  repository: IRepository;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router,
+              private _repositoryService: RepositoryService) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+
+    this._repositoryService.getRepository(id)
+      .subscribe(repo => this.repository = repo,
+        error => this.errorMessage = <any>error);
   }
 
+  onBack(): void {
+    this._router.navigate(['/repo']);
+  }
 }
